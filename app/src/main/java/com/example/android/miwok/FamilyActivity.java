5a59fbd9c2aa5c7e29ac72bf1ef25282bf2d15b1
BMontijo <juansonos@gmail.com>
2016-11-02 15:04:29 -0400

    Add functionality to FamilyActivity

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -17,12 +17,41 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
+
+        // create array for number words
+        List<Word> words = new ArrayList<>();
+
+        // populate array with Word objects for 1 - 10
+        words.add(new Word(getResources().getString(R.string.father), "әpә"));
+        words.add(new Word(getResources().getString(R.string.mother), "әṭa"));
+        words.add(new Word(getResources().getString(R.string.son), "angsi"));
+        words.add(new Word(getResources().getString(R.string.daughter), "tune"));
+        words.add(new Word(getResources().getString(R.string.older_brother), "taachi"));
+        words.add(new Word(getResources().getString(R.string.younger_brother), "chalitti"));
+        words.add(new Word(getResources().getString(R.string.older_sister), "teṭe"));
+        words.add(new Word(getResources().getString(R.string.younger_sister), "kolliti"));
+        words.add(new Word(getResources().getString(R.string.grandmother), "ama"));
+        words.add(new Word(getResources().getString(R.string.grandfather), "paapa"));
+
+        // Word adapter for handling array to list view
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        // Find list layout by id
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Populate listView using adapter
+        listView.setAdapter(adapter);
+
     }
 }
