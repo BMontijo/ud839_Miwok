f6bf72841c8e365b081beb052f5e3c84ee856e53
BMontijo <juansonos@gmail.com>
2016-11-09 21:29:07 -0500

    Add new Word constructor (for images), use new constructor in Family, Colors, and Numbers categories.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,6 +4,7 @@ package com.example.android.miwok;
  * Created by Brian on 11/1/2016.
  * Represents a vocabulary word in Miwok
  * Contains the Miwok and default transaltions for said word
+ * Contains associated image for numbers, family, and color categories
  */
 
 public class Word {
@@ -12,8 +13,11 @@ public class Word {
 
     // variable to hold default word
     private String defaultTranslation;
+	
+	// variable to hold image resource id
+	private int imageResourceId;
 
-    // public constructor for Word object
+    // public constructor for Word object - no image
     public Word(String defaultWord, String miwokWord) {
         // place defaultWord into defaultTranslation variable
         defaultTranslation = defaultWord;
@@ -21,6 +25,18 @@ public class Word {
         // place miwokWord into miwokTranslation variable
         miwokTraslation = miwokWord;
     }
+	
+	// public constructor for word object - with image
+	public Word(String defaultWord, String miwokWord, int imageId) {
+        // place defaultWord into defaultTranslation variable
+        defaultTranslation = defaultWord;
+
+        // place miwokWord into miwokTranslation variable
+        miwokTraslation = miwokWord;
+		
+		// place imageId into imageResourceId
+		imageResourceId = imageId;
+	}
 
     // function to return Miwok word
     public String getMiwokTraslation() {
@@ -31,5 +47,10 @@ public class Word {
     public String getDefaultTranslation() {
         return defaultTranslation;
     }
+	
+	// function to return image id
+	public int getImageResourceId() {
+		return imageResourceId;
+	}
 
 }
